02cb6196851c35d4a62d7584a20a0f8b6ff46602
Xavier B <xavierbatista52@gmail.com>
2022-06-15 05:47:36 -0500

    ok

---
+++ client/components/Home/FirstSection.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Searchbar from "./Searchbar";
+
+function FirstSection() {
+  return (
+    <div className="bg-neutral-900">
+      <div className="container py-12 lg:py-64 flex items-center justify-center lg:justify-between text-center lg:text-left flex-col lg:flex-row">
+        <div className="max-w-2xl">
+          <h1 className="text-4xl lg:text-6xl font-extrabold">
+            Make web development easy.
+          </h1>
+          <h2 className="text-base mt-3 lg:mt-4 mb-5 text-neutral-300">
+            Choose from neatly made code, components, templates, and more made
+            by experienced developers to use for your websites.
+          </h2>
+          <Searchbar />
+        </div>
+        <div className="relative h-96 aspect-square ml-9 hidden lg:inline-block">
+          <Image src="/homepageImage.png" alt="Preview" layout="fill" />
+        </div>
+        <div className="relative w-full max-w-2xl mt-10 aspect-[1088/653] lg:hidden">
+          <Image src="/homepageImage2.png" alt="Preview" layout="fill" />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default FirstSection;
