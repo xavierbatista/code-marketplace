787a0963d5d5a2e20a72459d8e6ba7742ac30aca
Xavier B <xavierbatista52@gmail.com>
2022-06-17 06:11:11 -0500

    ok

--- client/pages/search.tsx
+++ client/pages/search.tsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
+import ProductCard from "../components/ProductCard";
+
 function Search() {
+  const [products, setProducts] = useState([
+    "Product name",
+    "Product name",
+    "Product name",
+    "Product name",
+    "Product name",
+    "Product name",
+    "Product name",
+    "Product name",
+    "Product name",
+  ]);
+
   return (
     <main>
       <div>
-        <h1 className="mx-auto max-w-[90rem] py-6 px-4 text-3xl font-semibold">
+        <h1 className="mx-auto max-w-[90rem] py-6 px-4 text-5xl font-semibold">
           Search related words
         </h1>
       </div>
-      <div className="flex mx-auto max-w-[90rem] py-6 px-4">
-        <div></div>
-        <div className="">Search page</div>
+      <div>
+        <h1 className="mx-auto max-w-[90rem] pt-6 pb-2 px-4 text-xl font-semibold">
+          Filters
+        </h1>
+      </div>
+      <div className="flex mx-auto max-w-[90rem] pb-6 px-4">
+        <div className="w-48 shrink-0 bg-neutral-800 h-96 mr-12 rounded-md p-4">
+          Stuff
+        </div>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {products.map((product, i) => {
+            return <ProductCard caption={product} key={i} />;
+          })}
+        </div>
       </div>
     </main>
   );
