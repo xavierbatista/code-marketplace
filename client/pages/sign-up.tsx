d6a43e5fbf7df73cbc4d7a9e964c8af23ed7062b
Xavier B <xavierbatista52@gmail.com>
2022-07-04 04:40:17 -0500

    stuff

--- client/pages/sign-up.tsx
+++ client/pages/sign-up.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function signUp() {
   return (
-    <div className="max-w-[38rem] mx-auto mt-24 px-4">
+    <div className="max-w-[34rem] mx-auto mt-24 px-4">
       {/* LOGO */}
       <div className="flex justify-start items-center select-none">
         <div className="w-8 h-8 relative">
@@ -17,20 +18,31 @@ function signUp() {
           </div>
         </div>
       </div>
-      {/* LOGO */}
-      <div className="bg-neutral-700 mt-7 rounded-md p-9">
-        <h1 className="text-2xl font-medium">
-          Create your Code Marketplace account
+      {/* FORM */}
+      <div className="bg-neutral-800 mt-7 rounded-md p-14">
+        <h1 className="text-2xl text-center font-medium">
+          Create your account
         </h1>
         <form action="" className="flex justify-start flex-col">
-          <label htmlFor="">Email</label>
-          <input type="text" className="rounded-md text-black" />
-          <label htmlFor="">Password</label>
-          <input type="text" className="rounded-md text-black" />
-          <button className="bg-red-600 py-4 rounded-md">
-            Create an account
+          <label htmlFor="" className="mt-7">
+            Email
+          </label>
+          <input type="text" className="rounded-md text-black h-12 px-5 mt-2" />
+          <label htmlFor="" className="mt-7">
+            Password
+          </label>
+          <input type="text" className="rounded-md text-black h-12 px-5 mt-2" />
+          <button className="bg-red-600 py-4 rounded-md mt-7">
+            Create your account
           </button>
         </form>
+        <p className="mt-7">
+          Have an account?
+          <span> </span>
+          <Link href="">
+            <a className="text-red-400">Sign in</a>
+          </Link>
+        </p>
       </div>
     </div>
   );
