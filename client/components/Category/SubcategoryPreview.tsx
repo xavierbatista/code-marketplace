f40f8aaa5957c0d384cfca37adb5f8f3af12f948
Xavier B <xavierbatista52@gmail.com>
2022-07-05 19:15:36 -0500

    stuff

--- client/components/Category/SubcategoryPreview.tsx
+++ client/components/Category/SubcategoryPreview.tsx
@@ -15,13 +15,13 @@ function SubcategoryPreview() {
       <div className="flex justify-between mb-2">
         <Link href="#">
           <a>
-            <h2 className="text-xl font-semibold inline-block hover:text-neutral-400">
+            <h2 className="text-xl font-semibold inline-block">
               Subcategory
             </h2>
           </a>
         </Link>
         <Link href="#">
-          <a className="mt-auto text-sm hover:text-neutral-400">View all ➤</a>
+          <a className="mt-auto text-sm">View all ➤</a>
         </Link>
       </div>
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
