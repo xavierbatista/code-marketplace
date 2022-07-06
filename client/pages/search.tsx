bfe3286cac073c9f2f86378a825b092048af97f7
Xavier B <xavierbatista52@gmail.com>
2022-07-05 19:03:44 -0500

    stuff

--- client/pages/search.tsx
+++ client/pages/search.tsx
@@ -16,13 +16,13 @@ function Search() {
 
   return (
     <main>
-      <h1 className="mx-auto max-w-[90rem] my-8 px-4 text-5xl font-bold">
+      <h1 className="mx-auto max-w-[90rem] py-8 px-4 text-5xl font-bold">
         Search related words
       </h1>
-      <div className="mx-auto max-w-[90rem] mt-16 mb-2 px-4 text-xl font-semibold">
+      <div className="mx-auto max-w-[90rem] pt-8 pb-2 px-4 text-xl font-semibold">
         Filters
       </div>
-      <div className="flex mx-auto max-w-[90rem] mb-8 px-4">
+      <div className="flex mx-auto max-w-[90rem] pb-8 px-4">
         <div className="w-48 shrink-0 bg-neutral-800 h-fill mr-8 rounded-md p-4 hidden xl:inline-block"></div>
         <div className="w-full grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {products.map((product, i) => {
