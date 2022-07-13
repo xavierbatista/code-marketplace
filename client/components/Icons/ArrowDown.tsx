a2ebb4face54dcf262dd114355683051c3c6123f
Xavier B <xavierbatista52@gmail.com>
2022-07-12 21:02:00 -0500

    did stuff

--- client/components/Icons/ArrowDown.tsx
+++ client/components/Icons/ArrowDown.tsx
@@ -6,7 +6,7 @@ function ArrowDown() {
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
-      strokeWidth={2}
+      strokeWidth={3}
     >
       <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
     </svg>
