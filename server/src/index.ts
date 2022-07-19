a787035e77b9670e0a3dce5de6f611c616bc1f9a
Xavier B <xavierbatista52@gmail.com>
2022-07-18 19:43:51 -0500

    did stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,6 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-    
-});
+app.get("/", (req, res) => {});
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
