9427a27a5f4f51a243bf9149f35711b0fdc423d7
Xavier B <xavierbatista52@gmail.com>
2022-07-23 21:59:03 -0500

    did stuff

--- client/components/Navbar/Searchbar.tsx
+++ client/components/Navbar/Searchbar.tsx
@@ -1,23 +1,32 @@
 //NAVBAR SEARCHBAR
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 function Searchbar() {
+  const router = useRouter();
+  const pagesWithoutSearchbar = new Set(['/', '/cart', '/start-selling']);
+  const display = !pagesWithoutSearchbar.has(router.pathname);
+
   return (
-    <form
-      action=""
-      className="text-black text-sm flex justify-center items-center w-full px-4 lg:px-8"
-    >
-      <input
-        type="text"
-        placeholder=""
-        className="w-full h-10 px-5 rounded-l-full border-r-neutral-200 border"
-      />
-      <button className="bg-gray-100 h-full rounded-r-full px-4 flex items-center justify-center">
-        <div className="w-8 flex items-center justify-center h-10">
-          <Image src="/searchIcon.png" alt="Logo" width={18} height={18} />
-        </div>
-      </button>
-    </form>
+    <>
+      {display && (
+        <form
+          action=""
+          className="text-black text-sm flex justify-center items-center w-full px-4 lg:px-8"
+        >
+          <input
+            type="text"
+            placeholder=""
+            className="w-full h-10 px-5 rounded-l-md border-r-neutral-200 border"
+          />
+          <button className="bg-gray-100 h-full rounded-r-md px-4 flex items-center justify-center">
+            <div className="w-8 flex items-center justify-center h-10">
+              <Image src="/searchIcon.png" alt="Logo" width={18} height={18} />
+            </div>
+          </button>
+        </form>
+      )}
+    </>
   );
 }
 export default Searchbar;
